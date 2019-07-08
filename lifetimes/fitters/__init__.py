0c65e87cd7a1cde1c5aef959f050ea1758eb74f9
Philippe <philippefanaro@gmail.com>
2019-07-08 08:27:53 -0300

    initial superficial edit to the fitters' __init__.py

--- lifetimes/fitters/__init__.py
+++ lifetimes/fitters/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Base fitter for other classes."""
+
 import warnings
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
@@ -13,10 +14,17 @@ from ..utils import _save_obj_without_attr, ConvergenceError
 
 
 class BaseFitter(object):
-    """Base class for fitters."""
+    """
+    Base class for fitters.
+    """
 
-    def __repr__(self):
-        """Representation of fitter."""
+    def __repr__(
+        self
+    ):
+        """
+        Representation of fitter.
+        """
+
         classname = self.__class__.__name__
         try:
             subj_str = " fitted with {:d} subjects,".format(self.data.shape[0])
@@ -31,12 +39,26 @@ class BaseFitter(object):
         except AttributeError:
             return "<lifetimes.{classname}>".format(classname=classname)
 
-    def _unload_params(self, *args):
+    def _unload_params(
+        self, 
+        *args
+    ):
+        """
+        Unloads parameters.
+        """
+
         if not hasattr(self, "params_"):
             raise ValueError("Model has not been fit yet. Please call the .fit" " method first.")
+        
         return [self.params_[x] for x in args]
 
-    def save_model(self, path, save_data=True, save_generate_data_method=True, values_to_save=None):
+    def save_model(
+        self, 
+        path, 
+        save_data=True, 
+        save_generate_data_method=True, 
+        values_to_save=None
+    ):
         """
         Save model with dill package.
 
@@ -52,12 +74,15 @@ class BaseFitter(object):
         values_to_save: list, optional
             Placeholders for original attributes for saving object. If None
             will be extended to attr_list length like [None] * len(attr_list)
-
         """
+
         attr_list = ["data" * (not save_data), "generate_new_data" * (not save_generate_data_method)]
         _save_obj_without_attr(self, attr_list, path, values_to_save=values_to_save)
 
-    def load_model(self, path):
+    def load_model(
+        self, 
+        path
+    ):
         """
         Load model with dill package.
 
@@ -65,12 +90,14 @@ class BaseFitter(object):
         ----------
         path: str
             From what path load model.
-
         """
+
         with open(path, "rb") as in_file:
             self.__dict__.update(dill.load(in_file).__dict__)
 
-    def _compute_variance_matrix(self):
+    def _compute_variance_matrix(
+        self
+    ):
         params_ = self.params_
         return pd.DataFrame(
             (params_ ** 2).values * np.linalg.inv(self._hessian_) / self.data["weights"].sum(),
@@ -78,11 +105,24 @@ class BaseFitter(object):
             index=params_.index,
         )
 
-    def _compute_standard_errors(self):
+    def _compute_standard_errors(
+        self
+    ):
+        """
+        Computes the Standard Errors
+        """
+
         return np.sqrt(pd.Series(np.diag(self.variance_matrix_.values), index=self.params_.index))
 
-    def _compute_confidence_intervals(self):
+    def _compute_confidence_intervals(
+        self
+    ):
+        """
+        Computes the confidence intervals.
+        """
+
         inv_cdf_at_5_confidence = 1.96
+
         return pd.DataFrame(
             {
                 "lower 95% bound": self.params_ - inv_cdf_at_5_confidence * self.standard_errors_,
@@ -91,7 +131,20 @@ class BaseFitter(object):
             index=self.params_.index,
         )
 
-    def _fit(self, minimizing_function_args, initial_params, params_size, disp, tol=1e-7, bounds=None, **kwargs):
+    def _fit(
+        self, 
+        minimizing_function_args, 
+        initial_params, 
+        params_size, 
+        disp, 
+        tol=1e-7, 
+        bounds=None, 
+        **kwargs
+    ):
+        """
+        Fits the model usind ``scipy.optimize`` ``minimize`` function.
+        """
+
         # set options for minimize, if specified in kwargs will be overwritten
         minimize_options = {}
         minimize_options["disp"] = disp
@@ -121,7 +174,9 @@ class BaseFitter(object):
         )
 
     @property
-    def summary(self):
+    def summary(
+        self
+    ):
         """
         Summary statistics describing the fit.
 
@@ -134,9 +189,11 @@ class BaseFitter(object):
         --------
         ``print_summary``
         """
+
         df = pd.DataFrame(index=self.params_.index)
         df["coef"] = self.params_
         df["se(coef)"] = self.standard_errors_
         df["lower 95% bound"] = self.confidence_intervals_["lower 95% bound"]
         df["upper 95% bound"] = self.confidence_intervals_["upper 95% bound"]
+        
         return df
